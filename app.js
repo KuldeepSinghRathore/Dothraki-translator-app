6df22c1514308bbe8a8e6f7ec743e4f7217f02de
kuldeep singh <kuldeeprathore74@gmail.com>
2020-12-25 20:44:09 +0530

    api issue fixed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var btnTranslate = document.querySelector("#btn-translate");
+var btnTranslate = document.querySelector("#btn");
 
 var txtinput = document.querySelector("#txt-input");
 
@@ -6,30 +6,29 @@ var outputDiv = document.querySelector("#output");
 
 var serverApiUrl = "https://api.funtranslations.com/translate/dothraki.json";
 
-function getTranslation(text){
-    return serverApiUrl + "?" + "text=" + text
+function getTranslation(text) {
+  return serverApiUrl + "?" + "text=" + text;
 }
 
-function errorHandler(error){
-    console.log("error occured", error);
-    alert("Server issue Please try after sometime")
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Server issue Please try after sometime");
 }
 
-function clickEventHandler(){
-    var inputText = txtinput.value;
-  
+function clickEventHandler() {
+  var inputText = txtinput.value;
 
-//fetch starts
-fetch(getTranslation(inputText))
-.then(function responseHandler(response){
-    return response.json()
-})
-.then(function logJSON(json){
-    console.log(json);
-    translatedText = json.contents.translated;
-    outputDiv.innerText = translatedText;
-})
-.catch(errorHandler)
-};
+  //fetch starts
+  fetch(getTranslation(inputText))
+    .then(function responseHandler(response) {
+      return response.json();
+    })
+    .then(function logJSON(json) {
+      console.log(json);
+      translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
 
-btnTranslate.addEventListener("click", clickEventHandler );
+btn.addEventListener("click", clickEventHandler);
